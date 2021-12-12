40c00e8a5fdd4a92a51a0938008e742a4e8e571f
yashkumar patel <ysp18@scarletmail.rutgers.edu>
2021-12-12 10:47:35 -0500

    hotdogornothotdog end

--- hotdogOrNotHotdog/ViewController.swift
+++ hotdogOrNotHotdog/ViewController.swift
@@ -6,14 +6,127 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    let imagePicker = UIImagePickerController()
+    
+    var  result: [VNClassificationObservation] = []
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+    
+    
+    
+    
+    imagePicker.sourceType = .camera
+    imagePicker.allowsEditing = false
+    
+        present(imagePicker, animated: true, completion: nil)
+    
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        imagePicker.delegate = self
         // Do any additional setup after loading the view.
+        
+        
+        
+        
     }
+
+
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfodidFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey : Any])  {
+    
+    
+    
+    
+    if let image = info[.originalImage] as? UIImage {
+        
+        
+        imageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        guard let ciImage = CIImage(image : image) else {return}
+        
+        detect(image : ciImage)
+        
+        
+    }
+        
+        
+    
+}
+    
+func detect(image : CIImage) {
+    
+    
+    if let model = try? VNCoreMLModel(for : Inceptionv3().model){
+    
+        
+        let request = VNCoreMLRequest(model: model, completionHandler: { (request, error) in
+            
+            guard let results = request.results as? [VNClassificationObservation], let topResult = results.first else {return}
+            
+            
+            if topResult.identifier.contains("hotdog") {
+                
+                DispatchQueue.main.async {
+                    
+                    self.navigationItem.title = " Hotdog "
+                }
+            }
+            
+            else {
+                
+                
+                DispatchQueue.main.sync {
+                    self.navigationItem.title = " Not Hotdog "
+                }
+            }
+        })
+    
+        
+        let handler = VNImageRequestHandler(ciImage : image)
+        do {
+            try
+            handler.perform([request])
+        }
+        catch {
+            
+            print(error)
+        }
+        
+        
+        
+    
+    
+        
+        
+        
+        
+    
+        
+    }
+    
+    
+
 
 
 }
 
+}
